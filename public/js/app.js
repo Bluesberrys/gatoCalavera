0c5cb58cb409a6829c6295c861b9976b2b4890e3
Bluesberrys <bluesberrysden@gmail.com>
2025-05-13 17:42:57 -0600

    v0.2.103

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const burgerMenu = document.querySelector(".burger-menu");
-  const menu = document.querySelector(".menu");
-
-  burgerMenu.addEventListener("click", function () {
-    menu.classList.toggle("active");
-  });
-});
